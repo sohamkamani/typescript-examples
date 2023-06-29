6fc4db73a07f678837d815c0cc9cc7cda630b767
sohamkamani <sohamkamani@gmail.com>
2023-06-29 17:20:05 +0530

    add more examples for rest api post

--- rest-http-api-call/index.ts
+++ rest-http-api-call/index.ts
@@ -6,8 +6,24 @@ interface User {
 
 function getUsers(): Promise<User[]> {
 
-  // For now, consider the data is stored on a static `users.json` file
-  return fetch('./users.json')
+  // We can use the `Headers` constructor to create headers
+  // and assign it as the type of the `headers` variable
+  const headers: Headers = new Headers()
+  // Add a few headers
+  headers.set('Content-Type', 'application/json')
+  headers.set('Accept', 'application/json')
+  // Add a custom header, which we can use to check
+  headers.set('X-Custom-Header', 'CustomValue')
+
+  // Create the request object, which will be a RequestInfo type. 
+  // Here, we will pass in the URL as well as the options object as parameters.
+  const request: RequestInfo = new Request('./users.json', {
+    method: 'GET',
+    headers: headers
+  })
+
+  // For our example, the data is stored on a static `users.json` file
+  return fetch(request)
     // the JSON body is taken from the response
     .then(res => res.json())
     .then(res => {
@@ -25,4 +41,36 @@ if (!result) {
 getUsers()
   .then(users => {
     result.innerHTML = users.map(u => u.name).toString()
+  })
+
+// Send a POST request to create a new user, which takes a `User` object
+// as the parameter and returns a void promise (since we won't be using the result)
+function createUser(user: User): Promise<void> {
+  // Since this request will send JSON data in the body,
+  // we need to set the `Content-Type` header to `application/json`
+  const headers: Headers = new Headers()
+  headers.set('Content-Type', 'application/json')
+  // We also need to set the `Accept` header to `application/json`
+  // to tell the server that we expect JSON in response
+  headers.set('Accept', 'application/json')
+
+  const body: BodyInit = JSON.stringify(user)
+  const request: RequestInfo = new Request('/users', {
+    // We need to set the `method` to `POST` and assign the headers
+    method: 'POST',
+    headers: headers,
+    // Convert the user object to JSON and pass it as the body
+    body: body
+  })
+
+  // Send the request and print the response
+  return fetch(request)
+    .then(res => {
+      console.log("got response:", res)
+    })
+}
+
+createUser({ name: 'New User', age: '30' })
+  .then(() => {
+    console.log("User created!")
   })
